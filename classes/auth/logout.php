b22e3455160273dfd96dd92e400218a796c4e379
José Lucas <jose.esbellute@gmail.com>
2021-12-09 21:32:42 -0300

    Pequenos ajustes nos comentários

--- classes/auth/logout.php
+++ classes/auth/logout.php
@@ -1,7 +1,15 @@
 <?php
 
-//# Redireciona o usuário para o index após iniciar e destruir a sessão
+//-------------------------------------------------------------->
+//#~> Destrói a sessão
+//-------------------------------------------------------------->
+
 session_start();
+
 session_destroy();
+
 header('Location: ../../index.php');
+
 exit();
+
+//-------------------------------------------------------------->
--- classes/auth/validar_login.php
+++ classes/auth/validar_login.php
@@ -1,8 +1,13 @@
 <?php
 session_start();
 
-//# Redireciona o usuário para o index
-if(!$_SESSION['usuario']) {
+//-------------------------------------------------------------->
+//#~> Redireciona o usuário para o index
+//-------------------------------------------------------------->
+
+if (!$_SESSION['usuario']) {
     header('Location: ../index.php');
     exit();
 }
+
+//-------------------------------------------------------------->
