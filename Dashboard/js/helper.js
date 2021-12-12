1b44cb1641e82dc0faf4db603beb08309fd6bd2a
José Lucas <jose.esbellute@gmail.com>
2021-12-12 16:02:28 -0300

    Ajustes na limpeza dos campos

--- Dashboard/js/helper.js
+++ Dashboard/js/helper.js
@@ -1,19 +1,15 @@
-$(function () {
+window.onload = limparCampos();
 
-    //-------------------------------------------------------------->
-    
-    function limparCampos() {
+//-------------------------------------------------------------->
+
+function limparCampos() {
+
+    document.querySelector("#limpar_campos").onclick = function () {
+
         document.getElementById('nome_livro').value = null;
         document.getElementById('editora_livro').value = null;
-        document.getElementById('ano_publicacao').value = null;
+        document.getElementById('ano_publicacao_livro').value = null;
         document.getElementById('autor_livro').value = null;
         document.getElementById('descricao_livro').value = null;
     }
-
-    //-------------------------------------------------------------->
-
-
-    $('#limpar_campos').click(function () {
-        limparCampos();
-    });
-});
+}
